27e196b93afd1ffbb2410b1a24eeb9226b8db97a
choidz <tjdgusxx@gmail.com>
2022-01-27 00:25:41 +0900

    jekyll serve

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "[Git Error] Git requested URL returned Error 403 해결방법",
-        "excerpt":"발생이유 git the requested URL returend error : 403 이 발생하는 이유는 가지각색입니다. VSCode로 git push명령어를 썼을 때 에러가 발생했네요.. 제가 해결한 방법은 깃허브를 예전에 공부할 때 git push에 설정된 git hub의 ID/PW가 키체인에 등록되어 있어서 오류가 났던거였었네요. ## 해결방법 1.키체인 변경 1.Spotlight 검색에서 키체인접근 검색 후 실행 합니다. 2.github...","categories": ["GIT"],
+        "excerpt":"발생이유 git the requested URL returend error : 403 이 발생하는 이유는 가지각색입니다. VSCode로 git push명령어를 썼을 때 에러가 발생했네요.. 제가 해결한 방법은 깃허브를 예전에 공부할 때 git push에 설정된 git hub의 ID/PW가 키체인에 등록되어 있어서 오류가 났던거였었네요. 해결방법 1.키체인 변경 1.Spotlight 검색에서 키체인접근 검색 후 실행 합니다. 2.github 검색...","categories": ["git"],
         "tags": ["git error","403 error","git request error","git error on mac","맥북 git error"],
         "url": "/git/git-403-error/",
         "teaser": null
@@ -18,110 +18,134 @@ var store = [{
         "teaser": null
       },{
         "title": "[Algorithm] 자연수의 합",
-        "excerpt":"제시된 은 서로 다른 두 자연수 A, B를 입력하였을 때 A와 B 사이 자연수들의 합계를 구하는 알고리즘이다. 제시된 을 고려하여 아래 순서도의 괄호 안 내용에 가장 적합한 번호를 에서 선택하시오. 처리조건 입력되는 두 자연수의 경우 동일한 값은 입력되지 않는다. 합계는 작은 수에서 큰 수로 1씩 증가 시키며 구한다. 알고리즘에 사용된...","categories": ["Algorithm"],
+        "excerpt":"제시된 은 서로 다른 두 자연수 A, B를 입력하였을 때 A와 B 사이 자연수들의 합계를 구하는 알고리즘이다. 제시된 을 고려하여 아래 순서도의 괄호 안 내용에 가장 적합한 번호를 에서 선택하시오. 처리조건 입력되는 두 자연수의 경우 동일한 값은 입력되지 않는다. 합계는 작은 수에서 큰 수로 1씩 증가 시키며 구한다. 알고리즘에 사용된...","categories": ["algorithm"],
         "tags": ["자연수의 합","순서도","flowchart"],
         "url": "/algorithm/%EC%9E%90%EC%97%B0%EC%88%98%EC%9D%98-%ED%95%A9/",
         "teaser": null
       },{
         "title": "[Algorithm] 양음의 덧셈",
-        "excerpt":"1부터 100까지의 정수가“1-2+3-4+5….97-98+99-100”과 같이 전개될 때, 양수의 합과 음수의 합을 구분하여 출력하고자 한다. 제시된 을 참조하여 의 괄호안에 가장 적합한 항목을 에서 선택하시오. 처리조건 양수의 합은 변수 ODD, 음수의 합은 변수 EVEN을 사용하여 처리한다. 에서 /는 나누기 연산, X는 곱하기 연산을 의미한다 함수 INT()는 괄호 안의 연산을 행하되, 소수점 이하...","categories": ["Algorithm"],
+        "excerpt":"1부터 100까지의 정수가“1-2+3-4+5….97-98+99-100”과 같이 전개될 때, 양수의 합과 음수의 합을 구분하여 출력하고자 한다. 제시된 을 참조하여 의 괄호안에 가장 적합한 항목을 에서 선택하시오. 처리조건 양수의 합은 변수 ODD, 음수의 합은 변수 EVEN을 사용하여 처리한다. 에서 /는 나누기 연산, X는 곱하기 연산을 의미한다 함수 INT()는 괄호 안의 연산을 행하되, 소수점 이하...","categories": ["algorithm"],
         "tags": ["양음의 덧셈","순서도","flowchart"],
         "url": "/algorithm/%EC%96%91%EC%9D%8C%EC%9D%98-%EB%8D%A7%EC%85%88/",
         "teaser": null
       },{
         "title": "[Algorithm] 피보나치 수열의 합(1)",
-        "excerpt":"제 1항부터 제 N항까지 피보나치 수열의 합(S =1 + 1 + 2 + 3 + 5 + 8 + 13 + 21 +34 + 55 +…)을 계산하는 알고리즘이다. 제시된 을 참조하여 의 괄호안에 가장 적합한 항목을 에서 선택하시오. 처리조건 계산하려는 항의 수 N을 입력하여 처리하기로 한다.(단, N≥=10) 알고리즘에 사용된 변수...","categories": ["Algorithm"],
+        "excerpt":"제 1항부터 제 N항까지 피보나치 수열의 합(S =1 + 1 + 2 + 3 + 5 + 8 + 13 + 21 +34 + 55 +…)을 계산하는 알고리즘이다. 제시된 을 참조하여 의 괄호안에 가장 적합한 항목을 에서 선택하시오. 처리조건 계산하려는 항의 수 N을 입력하여 처리하기로 한다.(단, N≥=10) 알고리즘에 사용된 변수...","categories": ["algorithm"],
         "tags": ["피보나치 수열의 합","순서도","flowchart","피보나치","수열"],
         "url": "/algorithm/%ED%94%BC%EB%B3%B4%EB%82%98%EC%B9%98-%EC%88%98%EC%97%B4%EC%9D%98-%ED%95%A9(1)/",
         "teaser": null
       },{
         "title": "[Algorithm] 팩토리얼의 합",
-        "excerpt":"다음은 2!+3!+4!+…+19!+20!의 결과를 출력하는 알고리 즘이다. 제시된 을 참조하여 아래 순서도의 괄호 안 내용에 가장 적합한 항목을 에서 선택하시오. 처리조건 알고리즘에 사용된 변수는 아래와 같다. SUM : 합계변수 F : 각 항의 팩토리얼 결과 기억변수 I : 1증가 변수 2!+3!+4!+…+19!+20!의 결과 구하기 2!+3!+4!+5!+…+20! =(1×2)+(1×2×3)+…(1×2×3×4……..20) F = F * I (F=1...","categories": ["Algorithm"],
+        "excerpt":"다음은 2!+3!+4!+…+19!+20!의 결과를 출력하는 알고리 즘이다. 제시된 을 참조하여 아래 순서도의 괄호 안 내용에 가장 적합한 항목을 에서 선택하시오. 처리조건 알고리즘에 사용된 변수는 아래와 같다. SUM : 합계변수 F : 각 항의 팩토리얼 결과 기억변수 I : 1증가 변수 2!+3!+4!+…+19!+20!의 결과 구하기 2!+3!+4!+5!+…+20! =(1×2)+(1×2×3)+…(1×2×3×4……..20) F = F * I (F=1...","categories": ["algorithm"],
         "tags": ["팩토리얼의 합","순서도","flowchart","팩토리얼"],
         "url": "/algorithm/%ED%8C%A9%ED%86%A0%EB%A6%AC%EC%96%BC%EC%9D%98-%ED%95%A9/",
         "teaser": null
       },{
         "title": "[Algorithm] 피보나치 수열의 합(2)",
-        "excerpt":"두 개의 정수 A와 B를 입력받았을 때 A와 B 사이에 존재하는 피보나치 수와 그 개수를 출력하는 알고리즘이다. 제시된 을 참조하여 의 괄호안에 가장 적합한 항목을 에서 선택하시오. 처리조건 A와 B는 음이 아닌 정수이며, 3이상의 값을 입력하도록 한다 B가 A보다 큰 수로 고려한다. A,B : 범위를 지정하는 입력변수 F1,F2,C : 피보나치...","categories": ["Algorithm"],
+        "excerpt":"두 개의 정수 A와 B를 입력받았을 때 A와 B 사이에 존재하는 피보나치 수와 그 개수를 출력하는 알고리즘이다. 제시된 을 참조하여 의 괄호안에 가장 적합한 항목을 에서 선택하시오. 처리조건 A와 B는 음이 아닌 정수이며, 3이상의 값을 입력하도록 한다 B가 A보다 큰 수로 고려한다. A,B : 범위를 지정하는 입력변수 F1,F2,C : 피보나치...","categories": ["algorithm"],
         "tags": ["피보나치 수열의 합","순서도","flowchart","피보나치","수열"],
         "url": "/algorithm/%ED%94%BC%EB%B3%B4%EB%82%98%EC%B9%98-%EC%88%98%EC%97%B4%EC%9D%98-%ED%95%A9(2)/",
         "teaser": null
       },{
         "title": "[Algorithm] 진법변환(2진수 -> 10진수)",
-        "excerpt":"크기가 8인 배열 T(8)에 저장된 2진수를 10진수로 변환하여 출력하는 순서도이다. 다음의 을 참조하여 의 괄호 안에 가장 적 합한 항목을 에서 선택하시오.선택하시오. 처리조건 8비트 2진수 값의 최상위 비트는 부호 비트이며, 최상위 비트의 값이 0인 경우 양 수이고 1인 경우 음수이다(단, 음수의 경우 2의 보수로 표현된 값이다). 8비트 2진수의 값은 크기가...","categories": ["Algorithm"],
+        "excerpt":"크기가 8인 배열 T(8)에 저장된 2진수를 10진수로 변환하여 출력하는 순서도이다. 다음의 을 참조하여 의 괄호 안에 가장 적 합한 항목을 에서 선택하시오.선택하시오. 처리조건 8비트 2진수 값의 최상위 비트는 부호 비트이며, 최상위 비트의 값이 0인 경우 양 수이고 1인 경우 음수이다(단, 음수의 경우 2의 보수로 표현된 값이다). 8비트 2진수의 값은 크기가...","categories": ["algorithm"],
         "tags": ["진법변환","순서도","flowchart","2진수","10진수"],
         "url": "/algorithm/%EC%A7%84%EB%B2%95%EB%B3%80%ED%99%98(2%EC%A7%84%EC%88%98-10%EC%A7%84%EC%88%98)/",
         "teaser": null
       },{
         "title": "[Algorithm] 배수구하기",
-        "excerpt":"제시된 은 1에서 100까지의 정수 중에서 3의 배 수이면서 5의 배수인 수를 출력하는 알고리즘이다. 제시된 을 참조하여 아래 순서도의 괄호 안 내용에 가 장 적합한 항목을 에서 선택하시오. 처리조건 mod는 나누기 연산의 나머지를 구하는 연산자이다. 알고리즘에 사용된 변수는 아래와 같다. i : 인덱스 변수 SU1 : 3으로 나눈 나머지 저장...","categories": ["Algorithm"],
+        "excerpt":"제시된 은 1에서 100까지의 정수 중에서 3의 배 수이면서 5의 배수인 수를 출력하는 알고리즘이다. 제시된 을 참조하여 아래 순서도의 괄호 안 내용에 가 장 적합한 항목을 에서 선택하시오. 처리조건 mod는 나누기 연산의 나머지를 구하는 연산자이다. 알고리즘에 사용된 변수는 아래와 같다. i : 인덱스 변수 SU1 : 3으로 나눈 나머지 저장...","categories": ["algorithm"],
         "tags": ["배수 구하기","순서도","flowchart","배수"],
         "url": "/algorithm/%EB%B0%B0%EC%88%98%EA%B5%AC%ED%95%98%EA%B8%B0/",
         "teaser": null
       },{
         "title": "[Algorithm] 2차원배열(삼각행렬)",
-        "excerpt":"아래의 순서도는 5행, 5열의 2차원 배열에 아래와 같이 값을 입력하는 알고리즘이다. 2차원 배열의 저장 위치는 아래 왼쪽의 그림과 같고 순서도에 해당되는 처리 결과는 오른쪽의 2차원 배열의 형태로 저장된다. 제시된 을 참조하여 의 괄호 안에 가장 적합한 항목을 &lt; 답항보기&gt;에서 선택하시오. 처리조건 알고리즘에 사용된 변수는 아래와 같다. A(5,5) : 값을 저장하기...","categories": ["Algorithm"],
+        "excerpt":"아래의 순서도는 5행, 5열의 2차원 배열에 아래와 같이 값을 입력하는 알고리즘이다. 2차원 배열의 저장 위치는 아래 왼쪽의 그림과 같고 순서도에 해당되는 처리 결과는 오른쪽의 2차원 배열의 형태로 저장된다. 제시된 을 참조하여 의 괄호 안에 가장 적합한 항목을 &lt; 답항보기&gt;에서 선택하시오. 처리조건 알고리즘에 사용된 변수는 아래와 같다. A(5,5) : 값을 저장하기...","categories": ["algorithm"],
         "tags": ["2차원배열","삼각행렬","5행 5열 배열","배열","순서도","flowchart","배수"],
         "url": "/algorithm/2%EC%B0%A8%EC%9B%90%EB%B0%B0%EC%97%B4,%EC%82%BC%EA%B0%81%ED%96%89%EB%A0%AC/",
         "teaser": null
       },{
         "title": "[Algorithm] 2차원배열(지그재그)",
-        "excerpt":"4행 2열의 2차원 배열 A(4,5)에 과 같이 숫자를 기억시키고자 한다. 제시된 의 괄호에 가장 적합한 항목을 에서 선택하시오. 처리조건 배열 A(4,5)에 기억되는 내용은 다음과 같다. 알고리즘에 사용된 변수는 다음과 같다. A(4,5) : 데이터 저장 배열 R : 채워지는 값 K : 진행방향 지정 M, N : 열의 위치 지정 변수...","categories": ["Algorithm"],
+        "excerpt":"4행 2열의 2차원 배열 A(4,5)에 과 같이 숫자를 기억시키고자 한다. 제시된 의 괄호에 가장 적합한 항목을 에서 선택하시오. 처리조건 배열 A(4,5)에 기억되는 내용은 다음과 같다. 알고리즘에 사용된 변수는 다음과 같다. A(4,5) : 데이터 저장 배열 R : 채워지는 값 K : 진행방향 지정 M, N : 열의 위치 지정 변수...","categories": ["algorithm"],
         "tags": ["2차원배열","지그재그 배열","4행 5열 배열","배열","순서도","flowchart","배수"],
         "url": "/algorithm/2%EC%B0%A8%EC%9B%90%EB%B0%B0%EC%97%B4,%EC%A7%80%EA%B7%B8%EC%9E%AC%EA%B7%B8-%EB%B0%B0%EC%97%B4/",
         "teaser": null
       },{
         "title": "[Algorithm] 2차원배열(다이아몬드 배열)",
-        "excerpt":"5행 5열의 2차원 배열 A(5,5)에 과 같이 숫자를 기억시키고자 한다. 제시된 의 안 내용에 가장 적합한 항목을 에서 선택하시오. 처리조건 배열 A(5,5)에 기억되는 내용은 다음과 같다. 알고리즘에 사용된 변수는 다음과 같다. A(5,5) : 데이터 저장 배열 R1, R2 : 열의 시작과 끝 위치 지정 변수 S, J : 인덱스 변수...","categories": ["Algorithm"],
+        "excerpt":"5행 5열의 2차원 배열 A(5,5)에 과 같이 숫자를 기억시키고자 한다. 제시된 의 안 내용에 가장 적합한 항목을 에서 선택하시오. 처리조건 배열 A(5,5)에 기억되는 내용은 다음과 같다. 알고리즘에 사용된 변수는 다음과 같다. A(5,5) : 데이터 저장 배열 R1, R2 : 열의 시작과 끝 위치 지정 변수 S, J : 인덱스 변수...","categories": ["algorithm"],
         "tags": ["2차원배열","다이아몬드 배열","5행 5열 배열","배열","순서도","flowchart","다이아몬드"],
         "url": "/algorithm/%EB%8B%A4%EC%9D%B4%EC%95%84%EB%AA%AC%EB%93%9C%EB%B0%B0%EC%97%B4/",
         "teaser": null
       },{
         "title": "[Algorithm] 정렬 (버블정렬)",
-        "excerpt":"제시된 은 배열 A(10)에 기억된 10개의 수치 데이터에 대하여 버블 정렬(Bubble Sort)을 이용하여 오름차순으로 정렬 하는 순서도이다. 제시된 을 참조하여 의 괄 호 안 내용에 가장 적합한 항목을 에서 선택하시오 처리조건 배열 A(5,5)에 기억되는 내용은 다음과 같다. N : 정렬하고자 하는 수치 데이터의 갯수 i : 정렬의 회전수를 계산하기 위한...","categories": ["Algorithm"],
+        "excerpt":"제시된 은 배열 A(10)에 기억된 10개의 수치 데이터에 대하여 버블 정렬(Bubble Sort)을 이용하여 오름차순으로 정렬 하는 순서도이다. 제시된 을 참조하여 의 괄 호 안 내용에 가장 적합한 항목을 에서 선택하시오 처리조건 배열 A(5,5)에 기억되는 내용은 다음과 같다. N : 정렬하고자 하는 수치 데이터의 갯수 i : 정렬의 회전수를 계산하기 위한...","categories": ["algorithm"],
         "tags": ["버블정렬","정렬","배열","순서도","flowchart","bubble sort"],
         "url": "/algorithm/%EB%B2%84%EB%B8%94%EC%A0%95%EB%A0%AC/",
         "teaser": null
       },{
         "title": "[Algorithm] 정렬 (삽입정렬)",
-        "excerpt":"삽입 정렬(Insertion Sort) 기법을 사용하여 전산실 직원 20명의 토익 점수가 저장되어 있는 배열 TOE(20)의 데이 터를 오름차순으로 정렬하고자 한다. 제시된 을 참조하여 의 괄호 안 내용에 가장 적합한 항목 을 에서 선택하시오. 처리조건 알고리즘에 사용되는 변수들은 다음과 같다. TOE(20):20개의 데이터가 기억된 배열 I:인덱스 변수 J:인덱스 변수 KEY:비교 기준이 되는 키...","categories": ["Algorithm"],
+        "excerpt":"삽입 정렬(Insertion Sort) 기법을 사용하여 전산실 직원 20명의 토익 점수가 저장되어 있는 배열 TOE(20)의 데이 터를 오름차순으로 정렬하고자 한다. 제시된 을 참조하여 의 괄호 안 내용에 가장 적합한 항목 을 에서 선택하시오. 처리조건 알고리즘에 사용되는 변수들은 다음과 같다. TOE(20):20개의 데이터가 기억된 배열 I:인덱스 변수 J:인덱스 변수 KEY:비교 기준이 되는 키...","categories": ["algorithm"],
         "tags": ["삽입정렬","정렬","배열","순서도","flowchart","insertion sort"],
         "url": "/algorithm/%EC%82%BD%EC%9E%85%EC%A0%95%EB%A0%AC/",
         "teaser": null
       },{
         "title": "[Algorithm] 정렬 (석차구하기)",
-        "excerpt":"1차원 배열을 이용하여 학생 50명의 성명, 국어점수, 영어 점수, 수학점수를 입력받아 총점을 계산하고 계산된 총점으로 석차를 구하여 출력하는 알고리즘이다. 제시된 을 참조하여 의 괄호 안 내용에 가장 적합한 항목을 에서 선택하시오. 처리조건 알고리즘에 사용되는 변수들은 다음과 같다. NAM(50), K(50), E(50), M(50) : 성명, 국어점수, 영어점수, 수학점수 배열 SUM(50) : 총점배열...","categories": ["Algorithm"],
+        "excerpt":"1차원 배열을 이용하여 학생 50명의 성명, 국어점수, 영어 점수, 수학점수를 입력받아 총점을 계산하고 계산된 총점으로 석차를 구하여 출력하는 알고리즘이다. 제시된 을 참조하여 의 괄호 안 내용에 가장 적합한 항목을 에서 선택하시오. 처리조건 알고리즘에 사용되는 변수들은 다음과 같다. NAM(50), K(50), E(50), M(50) : 성명, 국어점수, 영어점수, 수학점수 배열 SUM(50) : 총점배열...","categories": ["algorithm"],
         "tags": ["석차구하기","정렬","배열","순서도","flowchart"],
         "url": "/algorithm/%EC%84%9D%EC%B0%A8%EA%B5%AC%ED%95%98%EA%B8%B0/",
         "teaser": null
       },{
         "title": "[Algorithm] 진수변환 (10진수->2진수)",
-        "excerpt":"다음 순서도는 0에서 999 사이의 임의의 10진 정수를 입 력받아 2진수로 변환하는 알고리즘이다. 제시된 을 참조하여 의 괄호 안 내용에 가장 적합한 항목을 에서 선택하시오. 처리조건 1.알고리즘에 사용된 변수는 아래와 같다. A(10) : 2진수 변환 후 저장을 위한 배열변수 I : 배열의 첨자변수 NUM : 10진 자료입력 및 몫 변수...","categories": ["Algorithm"],
+        "excerpt":"다음 순서도는 0에서 999 사이의 임의의 10진 정수를 입 력받아 2진수로 변환하는 알고리즘이다. 제시된 을 참조하여 의 괄호 안 내용에 가장 적합한 항목을 에서 선택하시오. 처리조건 1.알고리즘에 사용된 변수는 아래와 같다. A(10) : 2진수 변환 후 저장을 위한 배열변수 I : 배열의 첨자변수 NUM : 10진 자료입력 및 몫 변수...","categories": ["algorithm"],
         "tags": ["진수변환","10진수","2진수","변환","순서도","flowchart","알고리즘"],
         "url": "/algorithm/10%EC%A7%84%EC%88%98-2%EC%A7%84%EC%88%98-%EB%B3%80%ED%99%98/",
         "teaser": null
       },{
         "title": "[React] 리액트 기초 배우기 #1 Hello React! 리액트 프로젝트 생성과 빌드",
-        "excerpt":"💡 해당 글은 리액트에서 제공하는 자습서에 내용을 덧붙이고 풀어서 작성한 글입니다. 💡 원문 : https://ko.reactjs.org/docs/getting-started.html create-react-app 리액트 프로젝트를 만들기 위해선 creact-react-app을 이용합니다. npx create-react-app 프로젝트이름 저는 프로젝트이름을 react-tutorial으로 생성하였습니다. cd react-tutorial npm start 해당 폴더로 접근한 뒤 npm start로 실행할 수 있습니다. 명령어로 실행시키면 브라우저를 통해 자동으로 react가 로드됩니다. 💡...","categories": ["React"],
+        "excerpt":"💡 해당 글은 리액트에서 제공하는 자습서에 내용을 덧붙이고 풀어서 작성한 글입니다. 💡 원문 : https://ko.reactjs.org/docs/getting-started.html create-react-app 리액트 프로젝트를 만들기 위해선 creact-react-app을 이용합니다. npx create-react-app 프로젝트이름 저는 프로젝트이름을 react-tutorial으로 생성하였습니다. cd react-tutorial npm start 해당 폴더로 접근한 뒤 npm start로 실행할 수 있습니다. 명령어로 실행시키면 브라우저를 통해 자동으로 react가 로드됩니다. npx...","categories": ["react"],
         "tags": ["react","리액트","리액트 생성","리액트 기초","리액트 만들기","리액트 강의","JSX"],
         "url": "/react/react-1/",
         "teaser": null
       },{
         "title": "[React] 리액트 기초 배우기 #2 첫번째 컴포넌트 ",
-        "excerpt":"💡 해당 글은 리액트와 벨로퍼트님 블로그에서 제공하는 자습서에 내용을 덧붙이고 풀어서 작성한 글입니다. 💡 원문 : https://ko.reactjs.org/docs/getting-started.html 💡 벨로퍼트와 함께하는 모던 리액트 https://react.vlpt.us/basic/03-first-component.html Hello.js src 디렉토리에 Hello.js라는 새로운 파일을 생성 후 작성합니다. import React from 'react'; function Hello() { return &lt;div&gt;안녕하세요&lt;/div&gt; } export default Hello; 리액트 컴포넌트를 만들시에는 import React...","categories": ["React"],
+        "excerpt":"함수 컴포넌트와 클래스 컴포넌트 React를 사용할 때는 컴포넌트를 class 또는 함수로 정의할 수 있습니다. class로 정의된 컴포넌트는 많은 기능을 제공합니다. 클래스 컴포넌트로 정의하려면 React.Component를 상속받아야 합니다. 컴포넌트를 정의하는 가장 간단한 방법은 JavaScript 함수를 작성하는 것입니다. 함수 컴포넌트 props function Welcome(props) { return &lt;h1&gt;Hello, {props.name}&lt;/h1&gt;; } 이 함수는 데이터를 가진 하나의...","categories": ["react"],
         "tags": ["react","리액트","리액트 컴포넌트","react component","app.js","리액트 강의"],
         "url": "/react/react-2/",
         "teaser": null
       },{
         "title": "[React] 리액트 기초 배우기 #3 JSX ",
-        "excerpt":"💡 해당 글은 리액트와 벨로퍼트님 블로그에서 제공하는 자습서에 내용을 덧붙이고 풀어서 작성한 글입니다. 💡 원문 : https://ko.reactjs.org/docs/getting-started.html 💡 벨로퍼트와 함께하는 모던 리액트 https://react.vlpt.us/basic/03-first-component.html JSX 안에 자바스크립트 값 사용하기 저번 react #2에 이어서 JSX문법을 더 익혀보도록 하겠습니다. JSX 내부에 자바스크립트 변수를 보여줘야 할 때에는 {} 으로 감싸서 보여줍니다. App.js import React...","categories": ["React"],
+        "excerpt":"저번 react #2 컴포넌트 마지막 부분에 이어서 JSX문법을 더 익혀보도록 하겠습니다. JSX 내부에 자바스크립트 변수를 보여줘야 할 때에는 {} 으로 감싸서 보여줍니다. JSX 안에 자바스크립트 값 사용하기 App.js import React from 'react'; import Hello from './Hello'; const element = 'This is JSX' function App() { return ( &lt;div&gt; &lt;Hello /&gt;...","categories": ["react"],
         "tags": ["react","JSX","리액트 컴포넌트","react component","리액트 강의"],
         "url": "/react/react-3/",
         "teaser": null
       },{
         "title": "[React] 리액트 기초 배우기 #4 props ",
-        "excerpt":"💡 해당 글은 리액트와 벨로퍼트님 블로그에서 제공하는 자습서에 내용을 덧붙이고 풀어서 작성한 글입니다. 💡 원문 : https://ko.reactjs.org/docs/getting-started.html 💡 벨로퍼트와 함께하는 모던 리액트 https://react.vlpt.us/basic/03-first-component.html props의 사용 예를 들어서, App 컴포넌트에서 Hello 컴포넌트를 사용 할 때 name 이라는 값을 전달해주고 싶다고 가정해봅시다. 그러면, 이렇게 코드를 작성하면 됩니다. App.js import React from 'react';...","categories": ["React"],
-        "tags": ["react","componets","react props","properties","리액트 강의","framents"],
+        "excerpt":"props 는 properties 의 줄임말 (어떠한 값을 컴포넌트에게 전달해줘야 할 때 사용) 여러개의 props는 비구조화 할당으로 간결하게 작성 defaultProps 로 기본값 설정가능 컴포넌트 태그 사이에 넣은 값을 조회하고 싶을 땐, props.children props는 읽기 전용이다. 함수 컴포넌트나 클래스 컴포넌트 모두 컴포넌트의 자체 props를 수정하면 안됩니다, 예를들면 function sum(a, b) { return...","categories": ["react"],
+        "tags": ["react","componets","react props","properties","리액트 강의","fragments"],
         "url": "/react/react-4/",
+        "teaser": null
+      },{
+        "title": "[React] 리액트 기초 배우기 #5 조건부 렌더링 ",
+        "excerpt":"💡 해당 글은 벨로퍼트님 블로그와 리액트에서 제공하는 자습서에 내용을 덧붙이고 풀어서 작성한 글입니다. 💡 원문 : https://ko.reactjs.org/docs/getting-started.html 💡 벨로퍼트와 함께하는 모던 리액트 https://react.vlpt.us/basic/03-first-component.html 조건부 렌더링 우선 렌더링이란, HTML,CSS,Javascript 등 개발자가 작성한 문서가 브라우저에서 출력되는 과정을 말합니다. 렌더링에 관한 자세한 내용은 추후 포스팅을 하도록 하겠습니다. 개발자가 사이트를 제작할 때 어떠한 조건에...","categories": ["react"],
+        "tags": ["react","componets","react props","렌더링","리액트 렌더링","조건부 렌더링"],
+        "url": "/react/react-5/",
+        "teaser": null
+      },{
+        "title": "[React] 리액트 기초 배우기 #6 state ",
+        "excerpt":"state는 어떤 컴포넌트에만 한정하여 사용되는 데이터를 포함하며, 해당 데이터는 시간이 지남에 따라 변경될 수 있습니다. state는 사용자가 자유롭게 정의할 수 있으며, 일반적인 자바스크립트 객체이어야 합니다. 이번 포스팅에서는 읽기,쓰기 둘다 할 수 있는 state라는 개념에 대해 공부하겠습니다. (컴포넌트에서 보여줘야 하는 내용이 사용자 인터랙션에 따라 바뀌어야 할 때 어떻게 구현할 수 있는지)...","categories": ["react"],
+        "tags": ["react","componets","react state","state","리액트 강의"],
+        "url": "/react/react-6/",
+        "teaser": null
+      },{
+        "title": "[React] 리액트 기초 배우기 #7 생명주기 ",
+        "excerpt":"React의 생명 주기에 대해서 알아보겠습니다. React 컴포넌트는 생명 주기가 있습니다. 생애 주기 또는 라이프사이클(Life cycle)이라고도 많이 표현합니다. 컴포넌트가 실행되거나 업데이트되거나 제거될 때, 특정한 이벤트들이 발생합니다. 컴포넌트 생명주기 모든 컴포넌트는 여러 종류의 “생명주기 메서드”를 가지며, 이 메서드를 오버라이딩하여 특정 시점에 코드가 실행되도록 설정할 수 있습니다. 이 생명주기 도표를 필요할 때마다 활용하면...","categories": ["react"],
+        "tags": ["react","componets","react state","state","리액트 생명주기"],
+        "url": "/react/react-7/",
+        "teaser": null
+      },{
+        "title": "[React] 리액트 기초 배우기 #8 State와 생명주기를 이용하여 시계 만들기 ",
+        "excerpt":"이번 8장에서는 React의 State와 생명 주기의 응용으로 1초 단위로 숫자가 바뀌는 시계를 만들겠습니다. 📔 React 컴포넌트는 생명 주기가 있습니다. 📔 생애 주기 또는 라이프사이클(Life cycle)이라고도 많이 표현합니다. 📔 컴포넌트가 실행되거나 업데이트되거나 제거될 때, 특정한 이벤트들이 발생합니다. 1초 단위의 디지털 시계 만들기 Clock.js - Create Clock component src폴더 안에 Clock.js를 생성...","categories": ["react"],
+        "tags": ["react","componets","react state","state","리액트 생명주기","constructor","componentDidMount"],
+        "url": "/react/react-8/",
         "teaser": null
       }]
