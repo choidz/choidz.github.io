6cb16a595600890153483767d7c7aaa2dbecf753
choidz <you@example.com>
2025-12-17 21:26:24 +0900

    fix: 미사용 portfolioPath import 제거

--- src/blog/layout.js
+++ src/blog/layout.js
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
-import { blogPaths, portfolioPath } from "../routes/paths";
+import { blogPaths } from "../routes/paths";
 import PostList from "./post-list";
 import usePosts from "./usePosts";
 import { getPostViewsMap } from "./usePostViews";
